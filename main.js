20485ce739cfdd9b81bed8af683fca154d6ea3e0
C S MANODHITHYAA <manodhithyaa.cs@gmail.com>
2024-08-21 18:51:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function run() {
+    let html = document.querySelector("#html").value;
+    let css = document.querySelector("#css").value;
+    let js = document.querySelector("#js").value;
+    let output = document.querySelector("#output");
+
+    output.contentDocument.body.innerHTML = `${html}<style>${css}</style>`;
+    output.contentWindow.eval(js);
+}
+
+document.querySelector("#run").addEventListener("click", run);
